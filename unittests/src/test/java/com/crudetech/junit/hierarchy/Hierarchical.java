4f4afdbc7eb149b9157c38acc47765fc4d1d8737
Andreas Mueller <andreas.b.mueller@freenet.de>
2013-09-05 23:18:10 +0200

    -changed from suite to parent runner

--- unittests/src/test/java/com/crudetech/junit/hierarchy/Hierarchical.java
+++ unittests/src/test/java/com/crudetech/junit/hierarchy/Hierarchical.java
@@ -1,31 +1,56 @@
 package com.crudetech.junit.hierarchy;
 
+import org.junit.runner.Description;
 import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.BlockJUnit4ClassRunner;
-import org.junit.runners.Suite;
+import org.junit.runners.ParentRunner;
 import org.junit.runners.model.InitializationError;
 
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Hierarchical extends Suite {
+public class Hierarchical extends ParentRunner<Runner> {
+    private final List<Runner> childRunners;
 
-    public Hierarchical(Class<?> klass) throws InitializationError {
-        super(klass, computeTestAndSubTests(klass));
+    public Hierarchical(Class<?> testClass) throws InitializationError {
+        super(testClass);
+        childRunners = buildChildRunners();
     }
 
-    private static List<Runner> computeTestAndSubTests(Class<?> klass) throws InitializationError {
+    private List<Runner> buildChildRunners() throws InitializationError {
         List<Runner> runners = new ArrayList<Runner>();
-        HierarchicalBlockJUnit4ClassRunner outerRunner = new HierarchicalBlockJUnit4RootClassRunner(klass);
+        HierarchicalBlockJUnit4ClassRunner outerRunner = new HierarchicalBlockJUnit4RootClassRunner(getTestJavaClass());
         runners.add(outerRunner);
-        for (Class<?> innerClass : getAllInnerClasses(klass)) {
+        for (Class<?> innerClass : getAllInnerClasses(getTestJavaClass())) {
             BlockJUnit4ClassRunner innerRunner = isStatic(innerClass)
                     ? new BlockJUnit4ClassRunner(innerClass)
                     : new BlockJUnit4NonStaticClassRunner(innerClass, outerRunner);
             runners.add(innerRunner);
         }
         return runners;
+    }
+
+    private Class<?> getTestJavaClass() {
+        return getTestClass().getJavaClass();
+    }
+
+    @Override
+    protected Description describeChild(Runner child) {
+        return child.getDescription();
+    }
+
+
+    @Override
+    protected void runChild(Runner runner, final RunNotifier notifier) {
+        runner.run(notifier);
+    }
+
+
+    @Override
+    protected List<Runner> getChildren() {
+        return childRunners;
     }
 
     private static boolean isStatic(Class<?> innerClass) {
@@ -35,4 +60,5 @@ public class Hierarchical extends Suite {
     private static Class<?>[] getAllInnerClasses(Class<?> klass) {
         return klass.getClasses();
     }
+
 }
